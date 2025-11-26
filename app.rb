32c9063cad57bfd5fab1a5696c1cbd7d397f254d
Elias-johnsson <elias.johnsson5@outlook.com>
2025-11-26 16:18:52 +0100

    redigera och ta bort

--- app.rb
+++ app.rb
@@ -3,35 +3,47 @@ require 'sqlite3'
 require 'slim'
 require 'sinatra/reloader'
 
-
-
-# Routen /
-get '/todos' do
+def db
+  return @db if @db
   @db = SQLite3::Database.new("db/todos.db")
   @db.results_as_hash = true
-  @todos = @db.execute("SELECT * FROM todos")
+  return @db
+end
+
+get('/todos') do
+  @items = db.execute("SELECT * FROM todos")
   id = params[:id].to_i
   name = params[:name]
   desc = params[:desc]
   slim(:index)
 end
 
-post("/todos") do
-  @db = SQLite3::Database.new("db/todos.db")
-  @db.results_as_hash = true
-  @todos = @db.execute("SELECT * FROM todos")
+post('/todos') do
   name = params["new_name"]
   desc = params["new_description"]
-  @db.execute("INSERT INTO todos(name, desc) VALUES(?,?)",[name,desc])
+  db.execute("INSERT INTO todos(name, desc) VALUES(?,?)",[name,desc])
   redirect("/todos")
 end
 
-post("/todos/delete") do
-  @db = SQLite3::Database.new("db/todos.db")
-  @db.results_as_hash = true
-  to_delete = params["name"].to_i
-  @db.execute("DELETE FROM todos WHERE id = ?",to_delete)
-  redirect("/todos")
+get('/todos/:id/edit') do
+  id = params[:id].to_i
+  @edited = db.execute("SELECT * FROM todos WHERE id = ?",[id])
+  slim(:edit)
+end
+
+post('/todos/:id/update') do
+  id = params[:id].to_i
+  name = params[:name]
+  desc = params[:desc]
+  db.execute("UPDATE todos SET name= ?, desc=? WHERE id= ?",[name,desc,id])
+  redirect('/todos')
+end
+
+post('/todos/:id/delete') do
+  id = params[:id].to_i
+  p id
+  db.execute("DELETE FROM todos WHERE id = ?",[id])
+  redirect('/todos')
 end
 
 
